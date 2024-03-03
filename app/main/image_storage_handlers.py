69504603aace4aea2a1c017322be3205126ec2b3
bfpill <max@v3rv.com>
2024-03-03 14:04:01 +1100

    auth changes

--- app/main/image_storage_handlers.py
+++ app/main/image_storage_handlers.py
@@ -2,10 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, storage
 from io import BytesIO
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate('path/to/your/serviceAccountKey.json')  # Replace with your JSON credentials file
-    firebase_admin.initialize_app(cred, {'storageBucket': 'baggetters-38a7c.appspot.com'})  # Replace with your bucket name
-
 def store_image(image_id, data):
     data_blob = BytesIO(data)
     data_blob.seek(0)
--- app/main/settings.py
+++ app/main/settings.py
@@ -12,15 +12,14 @@ from firebase_admin import credentials
 
 load_dotenv()
 
-
 class Settings(BaseSettings):
   secret_key: str = "SECRET_KEY NOT SET"
   openai_api_key: str = "OPENAI_API_KEY NOT SET"
   email_pass: str = "EMAIL_PASS NOT SET"
   email_user: str = "EMAIL_USER NOT SET"
-  firebase_credentials_base64: str = "FIREBASE CRED NOT SET"
   master_password: str = "MASTER PASS NOT SET"
   pinecone_api_key: str = "PINECONE_API_KEY NOT SET" 
+  firebase_credentials: str = "FIREBASE_CREDENTIALS NOT SET"
 
   production: bool = False
   logger_file: str = 'surv.log'
@@ -34,11 +33,8 @@ settings = Settings()
 client = AsyncOpenAI(api_key=settings.openai_api_key)
 
 pc_key = settings.pinecone_api_key
-
-firebase_credentials_json = base64.b64decode(settings.firebase_credentials_base64)
-firebase_credentials = json.loads(firebase_credentials_json)
-
-cred = credentials.Certificate(firebase_credentials)
+print(settings.firebase_credentials)
+cred = credentials.Certificate(json.loads(str(settings.firebase_credentials)))
 firebase_admin.initialize_app(cred,  {'storageBucket': 'storysearch2.appspot.com'})
 
 # def getFireabseApp():
